3482baa5bc9a642ba64c10efb5bb0dc68c14d068
AxelJaBo <axel.910921@gmail.com>
2024-02-06 19:11:38 -0800

    builder.Services.AddScoped<IHelloWorldService, HelloWorldService>();

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -23,7 +23,7 @@ public class WeatherForecastController : ControllerBase
         {
                 ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
                 {
-                    Date = DateTime.Now.AddDays(index),
+                    // Date = DateTime.Now.AddDays(index),
                     TemperatureC = Random.Shared.Next(-20, 55),
                     Summary = Summaries[Random.Shared.Next(Summaries.Length)]
                 })
