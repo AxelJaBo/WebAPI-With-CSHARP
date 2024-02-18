58ee5e224ab0ced3f9f1cf59b42c2cad902c35e0
AxelJaBo <axel.910921@gmail.com>
2024-02-18 13:26:55 -0800

    Probando API con una base de datos SQL server

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using webapi;
 
 namespace webAPI.Controllers;
 
@@ -8,14 +9,25 @@ public class HelloWorldController : ControllerBase
 {
     IHelloWorldService helloWorldService;
 
-    public HelloWorldController(IHelloWorldService helloWorld)
+    TareasContext dbcontext;
+
+    public HelloWorldController(IHelloWorldService helloWorld, TareasContext db)
     {
         helloWorldService = helloWorld;
+        dbcontext = db;
     }
 
     [HttpGet]
     public IActionResult Get()
     {
         return Ok(helloWorldService.GetHelloWorld());
+    }
+
+    [HttpGet]
+    [Route("cretedb")]
+    public IActionResult CreateDatabase()
+    {
+        dbcontext.Database.EnsureCreated();
+        return Ok();
     }
 }
